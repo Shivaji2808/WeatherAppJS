ab892555c32dc21ae4e4322478b774fd1996e0fa
Shivaji Rathod <shivajirathod0015@gmail.com>
2023-09-28 22:21:19 +0530

    second commit

---
+++ ReadMe!!!.txt
@@ -0,0 +1,5 @@
+Enter the city name and click on "Get Weather" button or press enter.
+
+After that the dropdown menu will appear with all the weather details. 
+
+If the API doesn't respond or city name is incorrect then user's will see a prompt right beneath the search bar to enter the correct city name.
--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const apiKey = "5365f00bbbfa53ed46b0fbcec3551212";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+const airQualityApiUrl = "https://api.openweathermap.org/data/2.5/air_pollution?";
 const searchBox = document.querySelector(".search input");
-
 const searchBtn = document.querySelector(".search button");
-
 const weatherIcon = document.querySelector(".weather-icon");
+const unitToggle = document.getElementById("unit-toggle");
 
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+let selectedUnit = "metric";
+
+
+async function checkWeather(city) {
+    const url = `${apiUrl}q=${city}&units=${selectedUnit}&appid=${apiKey}`;
+  const response = await fetch(url);
 
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    }else{
-        var data = await response.json();
+    }else if(response.status == 502){
+        document.querySelector(".error").innerHTML = "The Server API is not working currently please try again later..."
+    }
+    else{
+        let data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
 
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+ "°C";
+    document.querySelector(".temp").innerHTML =
+    Math.round(data.main.temp) + (selectedUnit === "metric" ? "°C" : "°F");
 
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
 
@@ -30,8 +37,17 @@ async function checkWeather(city){
 
     document.querySelector(".lat").innerHTML = lat;
     document.querySelector(".lon").innerHTML = lon;
-    document.querySelector(".temphigh").innerHTML = Math.round(data.main.temp_max)+ "°C";
-    document.querySelector(".templow").innerHTML = Math.round(data.main.temp_min)+ "°C";
+    document.querySelector(".temphigh").innerHTML =
+    Math.round(data.main.temp_max) + (selectedUnit === "metric" ? "°C" : "°F");
+  document.querySelector(".templow").innerHTML =
+    Math.round(data.main.temp_min) + (selectedUnit === "metric" ? "°C" : "°F");
+
+
+unitToggle.addEventListener("change", () => {
+    selectedUnit = unitToggle.value;
+    const cityName = searchBox.value;
+    checkWeather(cityName);
+  });
 
     if(data.weather[0].main == "Clouds"){
         weatherIcon.src = "images/clouds.png";
@@ -48,20 +64,15 @@ async function checkWeather(city){
     document.querySelector(".error").style.display = "none";
     }
     
-    // weatherIcon.src = 'images/${(data.weather[0].main).toLowerCase()}.png'
-
-    
-
     aqiparam(lat,lon);
-
 }
 
 async function aqiparam(lat, lon) {
     const apiUrl2 = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     const response2 = await fetch(apiUrl2);
-    var data2 = await response2.json();
+    let data2 = await response2.json();
   
-    var airq = data2.list[0].main.aqi;
+    let airq = data2.list[0].main.aqi;
     if(airq == 1) var res = " (Good)";
     else if(airq == 2) var res = " (Fair)";
     else if(airq == 3) var res = " (Moderate)";
